4fa21c5f84140fb7888816a33cfb38935faeb7c4
KishanthDev <kishanthkisna@gmail.com>
2025-04-18 18:41:35 +0530

    feat: remove unused components and clean up Home page structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,17 +6,8 @@ import HeroSection from "@/components/Hero";
 import StatsSection from "@/components/Stats";
 import BigCommerceBenefits from "@/components/BigCommerceBenefits";
 
-const BusinessSolutions = dynamic(() => import("@/components/BusinessSolutions"));
-const Business = dynamic(() => import("@/components/Business"));
-const YourCompanySection = dynamic(() => import("@/components/CompanySection"));
-const Services = dynamic(() => import("@/components/Services"), {
-  loading: () => <p>Loading Services...</p>,
-});
-const Minzuno = dynamic(() => import("@/components/Minzuno"));
-const Contact = dynamic(() => import("@/components/Contact"));
 const Footer = dynamic(() => import("@/components/Footer"));
 
-///commit
 export default function Home() {
   return (
     <>
@@ -24,12 +15,6 @@ export default function Home() {
       <HeroSection />
       <hr />
       <StatsSection />
-      <BusinessSolutions />
-      <Business />
-      <YourCompanySection />
-      <Services />
-      <Minzuno />
-      <Contact />
       <BigCommerceBenefits />
       <Footer />
     </>
