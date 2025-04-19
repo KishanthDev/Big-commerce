2a6f50d0b6a65cc9f3295215a33b67b0b68f0ecc
KishanthDev <kishanthkisna@gmail.com>
2025-04-19 11:32:05 +0530

    feat: add Footer component to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import HeroSection from "@/components/Hero";
 import StatsSection from "@/components/Stats";
 import BigCommerceBenefits from "@/components/BigCommerceBenefits";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <hr />
       <StatsSection />
       <BigCommerceBenefits />
+      <Footer/>
     </>
   );
 }
