34dc2dfcbac148db6d43da4c3d20c4ca50cc9666
KishanthDev <kishanthkisna@gmail.com>
2025-04-28 17:04:32 +0530

    test: refactor Breadcrumb tests for improved structure and clarity; mock usePathname for accurate path rendering

--- src/components/breadcrumb/Breadcrumb.test.tsx
+++ src/components/breadcrumb/Breadcrumb.test.tsx
@@ -1,11 +1,16 @@
 import { render, screen } from "@testing-library/react";
+import { usePathname } from "next/navigation";
 import Breadcrumb from "./Breadcrumb";
 import { Category, Subcategory } from "../../../types/category";
+
+jest.mock("next/navigation", () => ({
+  usePathname: jest.fn(),
+}));
 
 describe("Breadcrumb Component", () => {
   const mockCategory: Category = {
     category: "Electronics",
-    subcategories: [{ name: "Laptops", businesses: [] }], // Added subcategories
+    subcategories: [{ name: "Laptops", businesses: [] }],
   };
   const mockSubcategory: Subcategory = { name: "Laptops", businesses: [] };
 
@@ -19,14 +24,26 @@ describe("Breadcrumb Component", () => {
     expect(screen.getByText("Categories")).toBeInTheDocument();
     expect(screen.getByText(mockCategory.category)).toHaveAttribute(
       "href",
-      "/subcategory/electronics",
+      "/subcategory/electronics"
     );
   });
 
   it("should render subcategory name if provided", () => {
     render(
-      <Breadcrumb category={mockCategory} subcategory={mockSubcategory} />,
+      <Breadcrumb category={mockCategory} subcategory={mockSubcategory} />
     );
     expect(screen.getByText(mockSubcategory.name)).toBeInTheDocument();
+  });
+
+  it("should render the category icon and name when on the category page", () => {
+    (usePathname as jest.Mock).mockReturnValue("/subcategory/electronics");
+
+    render(<Breadcrumb category={mockCategory} />);
+    
+    const categoryNameElement = screen.getByText(mockCategory.category);
+    
+    const iconElement = categoryNameElement.previousElementSibling;
+    
+    expect(iconElement).toHaveClass("mx-2");  
   });
 });
