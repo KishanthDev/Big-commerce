c0643d5934a5869f2584b3133232c4635265ab24
KishanthDev <kishanthkisna@gmail.com>
2025-04-29 12:44:49 +0530

    chatview eyecatcher html

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,16 +3,20 @@ import HeroSection from "@/components/contact/HeroSection";
 import ContactPage from "@/components/contact/ContactPage";
 import FAQSection from "@/components/contact/FAQSection";
 import React from "react";
+import Link from "next/link";
 import Header from "@/components/header/Header";
 import Footer from "@/components/footer/Footer";
 
 const Page = () => {
   return (
-    <div>
+    <div className="dark:bg-black bg-gray-100">
       <Header />
       <HeroSection />
       <ContactPage />
       <FAQSection />
+      <Link className="inline-block p-3 bg-blue-400 cursor-pointer rounded-2xl" href="/html/chatview.html">
+        Chat
+      </Link>
       <Footer />
     </div>
   );
