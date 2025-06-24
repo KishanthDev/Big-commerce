a1c72fa65cea348e1c42ef65c0ff719ffaaaa5c4
KishanthDev <kishanthkisna@gmail.com>
2025-06-24 16:33:38 +0530

    refactor: remove background opacity from ModalWrapper component

--- src/app/(api)/clerkauth/ModalWrapper.tsx
+++ src/app/(api)/clerkauth/ModalWrapper.tsx
@@ -16,7 +16,7 @@ const ModalWrapper: React.FC<ModalWrapperProps> = ({
   setOtp,
 }) => {
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-white bg-opacity-60 z-50">
+    <div className="fixed inset-0 flex items-center justify-center z-50">
       {/* Modal container */}
       <div className="bg-white rounded-lg p-8 w-full max-w-lg shadow-2xl">
         {/* Header */}
